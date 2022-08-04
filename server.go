33b9cb3e3196012db649538e376fd843296bb55b
changediff <changediff@outlook.com>
2022-08-05 00:16:41 +0800

    v0.2

--- server.go
+++ server.go
@@ -3,24 +3,68 @@ package main
 import (
 	"fmt"
 	"net"
+	"sync"
 )
 
 type Server struct {
 	Ip   string
 	Port int
+
+	//在线用户列表
+	OnlineMap map[string]*User
+	mapLock   sync.RWMutex
+
+	//消息广播的channel
+	Message chan string
 }
 
 //创建server接口
 func NewServer(ip string, port int) *Server {
 	server := &Server{
-		Ip:   ip,
-		Port: port,
+		Ip:        ip,
+		Port:      port,
+		OnlineMap: make(map[string]*User),
+		Message:   make(chan string),
 	}
 	return server
 }
 
+//监听Message，有消息就发送给所有User
+func (this *Server) ListenMessager() {
+	for {
+		msg := <-this.Message
+
+		//将msg发送给全部在线User
+		this.mapLock.Lock()
+		for _, cli := range this.OnlineMap {
+			cli.C <- msg
+		}
+		this.mapLock.Unlock()
+	}
+}
+
+// 广播消息
+func (this *Server) Broadcast(user *User, msg string) {
+	sendMsg := "[" + user.Addr + "]" + user.Name + ":" + msg
+	this.Message <- sendMsg
+}
+
 func (this *Server) Handler(conn net.Conn) {
 	//...当前连接任务
+	// fmt.Println("当前链接成功")
+
+	user := NewUser(conn)
+
+	//用户上线，将用户加入到onlineMap中
+	this.mapLock.Lock()
+	this.OnlineMap[user.Name] = user
+	this.mapLock.Unlock()
+
+	//广播上线消息
+	this.Broadcast(user, "已上线")
+
+	//当前handler阻塞
+	select {}
 }
 
 //启动服务器的接口
@@ -34,6 +78,9 @@ func (this *Server) Start() {
 
 	// close listen socket
 	defer listener.Close()
+
+	//启动监听Message的goroutine
+	go this.ListenMessager()
 
 	for {
 		// accept
