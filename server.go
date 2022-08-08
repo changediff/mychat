aae77670a042c320dda39e14b87a1415d2a04c31
changediff <changediff@outlook.com>
2022-08-08 22:43:21 +0800

    v0.3

--- server.go
+++ server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net"
 	"sync"
 )
@@ -62,6 +63,29 @@ func (this *Server) Handler(conn net.Conn) {
 
 	//广播上线消息
 	this.Broadcast(user, "已上线")
+
+	//接收客户端发送的消息
+	go func() {
+		buf := make([]byte, 4096)
+		for {
+			n, err := conn.Read(buf)
+			if n == 0 {
+				this.Broadcast(user, "下线")
+				return
+			}
+
+			if err != nil && err != io.EOF {
+				fmt.Println("Conn Read err: ", err)
+				return
+			}
+
+			//提取用户的消息，去除"\n"
+			msg := string(buf[:n-1])
+
+			//广播用户消息
+			this.Broadcast(user, msg)
+		}
+	}()
 
 	//当前handler阻塞
 	select {}
